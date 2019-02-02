b8cb47075a0c54217cb13755ec94806bf13297f8
Mohd Nazmus Saquib <nsaquib96@gmail.com>
2019-02-02 00:36:55 -0500

    Added Typed Heading and added profile image and added icon to the downloadable link

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  var typed = new Typed(".typed", {
+  strings: ["Web Developer.", "Business Analyst.", "Student."],
+  typeSpeed: 100,
+  loop: true,
+  startDelay: 1000,
+  showCursor: false
+});
+
+});
